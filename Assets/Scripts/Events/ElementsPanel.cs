42387aa7ed3a4a4e6400140e22807e30a8795cd6
kpogodzinski <kacperpogodzinski@spoko.pl>
2025-10-16 10:30:58 +0200

    Obracanie "+" na przycisku przy otwieraniu/zamykaniu panelu.

--- Assets/Scripts/Events/ElementsPanel.cs
+++ Assets/Scripts/Events/ElementsPanel.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class ElementsPanel : MonoBehaviour
@@ -10,11 +11,14 @@ public class ElementsPanel : MonoBehaviour
     private bool visible;
     private float animationLength;
     private float width;
+    private TextMeshProUGUI buttonText;
 
     private void Awake()
     {
         visible = false;
         animationLength = 0.3f;
+
+        buttonText = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
 
         Vector3[] corners = new Vector3[4];
         RectTransform rt = elementsPanel.GetComponent<RectTransform>();
@@ -25,10 +29,12 @@ public class ElementsPanel : MonoBehaviour
     private IEnumerator SlidePanel(int direction) // direction: -1 = left, 1 = right
     {
         Vector2 oldButtonPosition = transform.position;
+        Quaternion oldTextRotation = buttonText.transform.rotation;
         Vector2 oldPanelPosition = elementsPanel.transform.position;
         
 
         Vector2 newButtonPosition = oldButtonPosition + Vector2.right * width * direction;
+        Quaternion newTextRotation = oldTextRotation * Quaternion.Euler(0, 0, -direction * 45);
         Vector2 newPanelPosition = oldPanelPosition + Vector2.right * width * direction;
 
         float elapsedTime = 0;
@@ -36,12 +42,14 @@ public class ElementsPanel : MonoBehaviour
         {
             transform.position = Vector2.Lerp(oldButtonPosition, newButtonPosition, elapsedTime / animationLength);
             elementsPanel.transform.position = Vector2.Lerp(oldPanelPosition, newPanelPosition, elapsedTime / animationLength);
+            buttonText.transform.rotation = Quaternion.Lerp(oldTextRotation, newTextRotation, elapsedTime / animationLength);
             elapsedTime += Time.deltaTime;
             yield return null;
         }
 
         transform.position = newButtonPosition;
         elementsPanel.transform.position = newPanelPosition;
+        buttonText.transform.rotation = newTextRotation;
         //yield return null;
         visible = !visible;
     }
